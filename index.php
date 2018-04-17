c8bf4b297866c00f2a3242373d6ea1a6116237b4
happyandhappy <greyfrapps@gmail.com>
2018-04-17 18:43:35 +0100

    add class

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once("init.php");
 // $clientSecret = "44b96ad1ab9c32357decb79266a30fc03ed38eb483";
 // $client_id = "1000.4H6Z54RLYWBS70321WEUN90IZJP5NJ";
 // $client_secret = "85dce0fa056b2c3dc0d1b9b02bb8db89890da549aa";
-$client_id =="1000.RZDTYZTPA79S82954ANS4KLOW6DYIL";
-$client_secret = "46ba9a1177cc0f61a061b8226f8ccd832da766a8bc";
+// $client_id =="1000.RZDTYZTPA79S82954ANS4KLOW6DYIL";
+// $client_secret = "46ba9a1177cc0f61a061b8226f8ccd832da766a8bc";
 
 
 goToAuthUrl();
